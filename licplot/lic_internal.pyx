ec4b489f9ad039f3f6ffb0a708800839c46cb150
mahdi91 <31503195+mahdi91@users.noreply.github.com>
2022-03-18 18:30:49 +0100

    Update lic_internal.pyx

--- licplot/lic_internal.pyx
+++ licplot/lic_internal.pyx
@@ -45,14 +45,14 @@ cdef void _advance(float vx, float vy,
     elif vx<0:
         tx = -fx[0]/vx
     else:
-        zeros += 10
+        zeros += 1
         tx = 1e100
     if vy>0:
         ty = (1-fy[0])/vy
     elif vy<0:
         ty = -fy[0]/vy
     else:
-        zeros += 10
+        zeros += 1
         ty = 1e100
 
     if zeros==2:
